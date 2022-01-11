f9efc1c54de86925f60025e13115d8872239fc84
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2022-01-11 20:16:14 +0530

    Async
    Done

--- fsAsync/index.js
+++ fsAsync/index.js
@@ -3,3 +3,12 @@ const fs = require("fs");
 fs.writeFile("Botwrite.txt", "hello bots", (err) => {
     console.log("covered");
 });
+
+//fs.appendFile("Botwrite.txt", "Added new", (err) => {
+  //      console.log("Updated error");
+     
+//});
+
+fs.readFile("Botwrite.txt", "UTF-8", (err, data) => {
+    console.log(data);
+});
