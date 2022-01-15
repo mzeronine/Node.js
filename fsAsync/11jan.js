5b63208763e4b6f7afcb9b9764b0175337db0e38
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2022-01-15 11:23:18 +0530

    Update 11jan.js

--- fsAsync/11jan.js
+++ fsAsync/11jan.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
 
-fs.writeFile("Iambot", "Hello People", (err) => {
+fs.writeFile("Hello.txt", "This is bot", (err) => {
         console.log("Noted");
+        console.log(err);
+});
+
+fs.readFile("Hello.txt", "UTF-8", (err, data) => {
+        console.log(data);
+        console.log("Reading Done");
 
 });
 
-console.log("Hello, People first");
+fs.unlink("Hello.txt", (err) => {
+        console.log("Deleted");
+
+});
