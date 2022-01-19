aae3b51d1752138d23e8b3f87f16558b728fb70b
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2022-01-19 20:26:30 +0530

    path module

--- Pathmodule/path.js
+++ Pathmodule/path.js
@@ -3,3 +3,8 @@ const path = require("path");
 console.log(path.dirname('/Users/meghan/Desktop/Code/Wbsite/Node.js/Pathmodule/path.js'));
 console.log(path.extname('/Users/meghan/Desktop/Code/Wbsite/Node.js/Pathmodule/path.js'));
 console.log(path.basename('/Users/meghan/Desktop/Code/Wbsite/Node.js/Pathmodule/path.js'));
+
+ console.log(path.parse('/Users/meghan/Desktop/Code/Wbsite/Node.js/Pathmodule/path.js'));
+
+const myfavpath = path.parse(('/Users/meghan/Desktop/Code/Wbsite/Node.js/Pathmodule/path.js'));
+console.log(myfavpath.name);
