481fc734e22a0aa044613f4e5322ecae5dc9d6c8
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2022-02-02 22:23:41 +0530

    interesting and fun
    Import and Export own modules

---
+++ Import&Export/index.js
@@ -0,0 +1,5 @@
+const {add,sub,Name} = require("./oper");
+
+console.log(add(4,6));
+console.log(sub(7,3));
+console.log(Name);
---
+++ Import&Export/oper.js
@@ -0,0 +1,14 @@
+const add = (a,b) => {
+        return a+b;
+
+};
+
+const sub = (m,n) => {
+    return m-n;
+}
+
+const Name = "Hello";
+
+module.exports = {add, sub, Name};
+
+
