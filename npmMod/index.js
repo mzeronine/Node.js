b2da9921ae9afc3439780f4bc3588e4bb2c717ba
Meghan Vaze <vaze.2114100366@mujonline.edu.in>
2022-02-07 13:06:38 +0530

    validator

--- npmMod/index.js
+++ npmMod/index.js
@@ -1,2 +1,8 @@
 const chalk = require("chalk");
 console.log(chalk.green("Success"));
+
+const validator = require("validator");
+const res = validator.isEmail("meghan.com");
+console.log(res ? chalk.green(res) : chalk.red(res));
+const pho = validator.isMobile("99889");
+console.log(pho ? chalk.green(pho) : chalk.red(pho));
